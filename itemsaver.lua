465ad9f0f8a0a6d509e7d511a3b78f875e7a2a0d
valpleaderboard-lab <valpleaderboard@gmail.com>
2025-12-10 13:32:27 +0300

    Create itemsaver.lua

---
+++ itemsaver.lua
@@ -0,0 +1,119 @@
+local TextChatService = game:GetService("TextChatService")
+local RBXSystem = TextChatService.TextChannels.RBXSystem
+
+local Items = Instance.new("Folder", game.ReplicatedStorage)
+Items.Name = "Items"
+
+local Item = Instance.new("Folder", Items)
+Item.Name = "Item"
+
+local function onToolAdded(obj)
+	if obj:HasTag("Weapon") and not Item:FindFirstChild(obj:GetAttribute("ItemID")) then
+		pcall(function()
+			task.wait(0.5)
+			obj = obj:Clone()
+			for i, v in obj:GetDescendants() do
+				if (v:IsA("Sound") and v.Name == "Reload" or v.Name == "Gunshot") 
+					or (v:IsA("LuaSourceContainer") and v.Name == "KnifeServer" or v.Name == "KnifeLocal" or v.Name == "Maid" or v.Name == "FirstPerson" or v.Name == "KnifeClient")
+					or (v:IsA("Folder") and v.Name == "Animations")
+					or (v:IsA("RemoteEvent") and v.Name == "End" or v.Name == "Stab" or v.Name == "Throw")
+					or (v:IsA("BoolValue") and v.Name == "IsGun")
+					or v:IsA("TouchTransmitter")
+					or v:IsA("ParticleEmitter") then
+					v:Destroy()
+				end
+				if v:IsA("Decal") and v.Name == "Chroma" then
+					v:SetAttribute("ChromaLayer")
+					v.Color3 = Color3.fromRGB(255, 255, 255)
+				end
+			end
+
+			obj:RemoveTag("Weapon")
+			obj:RemoveTag(`Weapon_{obj:GetAttribute("ItemType")}`)
+			obj.Name = obj:GetAttribute("ItemID")
+			obj.Parent = Item
+			task.wait(0.1)
+
+			for i, v in obj:GetAttributes() do
+				obj:SetAttribute(i)
+			end
+			RBXSystem:DisplaySystemMessage(`<font color="#00FF00">{obj.Name} saved successfully.</font>`)
+		end)
+	end
+end
+
+local Radios = Instance.new("Folder", Items)
+Radios.Name = "Radios"
+
+local Effects = Instance.new("Folder", Items)
+Effects.Name = "Effects"
+
+local function checkItems(Player, obj)
+	local EffectName = Player:GetAttribute("EquippedEffect")
+
+	if EffectName and EffectName ~= "Dual" and EffectName ~= "None" and not Effects:FindFirstChild(EffectName) then
+		pcall(function()
+			task.wait(0.5)
+			local EffectFolder = Instance.new("Folder")
+			EffectFolder.Name = EffectName
+			EffectFolder.Parent = Effects
+
+			for _, v in Player.Character.DisplayRefKnife.Value:GetDescendants() do
+				if v:IsA("ParticleEmitter") or v:IsA("Fire") then
+					v:Clone().Parent = EffectFolder
+				end
+			end
+			
+			RBXSystem:DisplaySystemMessage(`<font color="#00FF00">{EffectFolder.Name} saved successfully.</font>`)
+		end)
+	end
+
+	local Radio = Player.Character:FindFirstChild("Radio")
+	local RadioName = Player:GetAttribute("EquippedRadio")
+
+	if Radio and RadioName and not Radios:FindFirstChild(RadioName) then
+		pcall(function()
+			task.wait(0.5)
+			obj = Radio:Clone()
+
+			if obj:FindFirstChild("OriginalSize") then
+				obj.Size = obj:FindFirstChild("OriginalSize").Value
+			end
+			
+			for i, v in obj:GetDescendants() do
+				if v:IsA("Sound") or v:IsA("Vector3Value") then
+					v:Destroy()
+				end
+			end
+
+			obj.Name = RadioName
+			obj.CFrame = CFrame.new(0, 0, 0)
+			obj.Parent = Radios
+			
+			RBXSystem:DisplaySystemMessage(`<font color="#00FF00">{obj.Name} saved successfully.</font>`)
+		end)
+	end
+end
+
+local function checkBackpack(Player)
+	pcall(function()
+		Player.Backpack.ChildAdded:Connect(onToolAdded)
+		Player.ChildAdded:Connect(function(obj)
+			if obj:IsA("Backpack") then
+				obj.ChildAdded:Connect(onToolAdded)
+			end
+		end)
+	end)
+end
+
+for i, Player in game.Players:GetPlayers() do
+	Player:GetAttributeChangedSignal("EquippedRadio"):Connect(function()
+		checkItems(Player)
+	end)
+	Player:GetAttributeChangedSignal("EquippedEffect"):Connect(function()
+		checkItems(Player)
+	end)
+	checkItems(Player)
+	checkBackpack(Player)
+end
+game.Players.PlayerAdded:Connect(checkBackpack,checkItems)
