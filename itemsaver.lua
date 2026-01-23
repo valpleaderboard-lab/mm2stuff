16d5f33cc0e5bb5eb9a10af8f27c3c252e2b1817
valpleaderboard-lab <valpleaderboard@gmail.com>
2026-01-23 21:51:04 +0300

    Update itemsaver.lua

--- itemsaver.lua
+++ itemsaver.lua
@@ -145,7 +145,7 @@ end)
 
 local Maps = Instance.new("Folder", Items)
 Maps.Name = "Maps"
-[workspace.ChildAdded:Connect(function(child)
+workspace.ChildAdded:Connect(function(child)
 	if child:HasTag("CurrentMap") then
 		pcall(function()
 			if not Maps:FindFirstChild(child.Name) then
